570b67edcf36ecdc0dac025c23e8effab0baa2d4
it.chhaya <it.chhaya@gmail.com>
2024-06-08 15:43:26 +0700

    add multiple user security

--- src/main/java/co/istad/mbanking/security/SecurityConfig.java
+++ src/main/java/co/istad/mbanking/security/SecurityConfig.java
@@ -1,14 +1,47 @@
 package co.istad.mbanking.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Bean
+    InMemoryUserDetailsManager configureUserSecurity() {
+        UserDetails admin = User
+                .withUsername("admin")
+                .password(passwordEncoder.encode("admin123"))
+                .roles("USER", "ADMIN")
+                .build();
+        UserDetails editor = User
+                .withUsername("editor")
+                .password(passwordEncoder.encode("editor123"))
+                .roles("USER", "EDITOR")
+                .build();
+        UserDetails subscriber = User
+                .withUsername("subscriber")
+                .password(passwordEncoder.encode("subscriber123"))
+                .roles("USER", "SUBSCRIBER")
+                .build();
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+        manager.createUser(admin);
+        manager.createUser(editor);
+        manager.createUser(subscriber);
+
+        return manager;
+    }
 
     @Bean
     SecurityFilterChain configureApiSecurity(HttpSecurity http) throws Exception {
@@ -22,7 +55,7 @@ public class SecurityConfig {
         // HTTP Basic Auth (Username & Password)
         http.httpBasic(Customizer.withDefaults());
 
-        // Disable CSRF Token
+        // Disable CSRF (Cross Site Request Forgery) Token
         http.csrf(token -> token.disable());
 
         // Make Stateless Session
